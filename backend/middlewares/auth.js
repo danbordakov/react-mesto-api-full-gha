1764bc63ca581d0eb0af3df6568e417a5499431f
danbordakov <danbordakov@yandex.ru>
2024-01-17 13:19:19 +0300

    ПР15

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require("jsonwebtoken");
 const UnauthorizedError = require("../errors/unauthorized-error");
